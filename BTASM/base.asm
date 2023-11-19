689bb16eca60a80550e6255718e3ab98883120a6
huuvuno1 <nguyenhuuvuno1@gmail.com>
2023-11-20 01:14:38 +0700

    update codebase & remove unused file

--- BTASM/base.asm
+++ BTASM/base.asm
@@ -3,14 +3,23 @@ include lib1.asm
 .stack 100h
 .data
   ;msg db 13, 10, 'Hello, World!$'
+  tiep_tuc db 13, 10, 'Tiep tuc? (c/k): $'
 
 .code
 MAIN:
   mov ax, @data
   mov ds, ax
+  CLRSCR
   
   
-  
+TIEP_TUC_LABEL:
+  HienString tiep_tuc
+  mov ah, 1
+  int     21h
+  cmp al, 'c'
+  jne EXIT
+  jmp MAIN
+
 EXIT:
   mov ax, 4c00h
   int 21h
